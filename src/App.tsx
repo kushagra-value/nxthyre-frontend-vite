088ec5adfa8a8dfd4211d4a10894b12dc6708f7e
HarshValuebound <harsh@valuebound.com>
2025-07-20 01:49:43 +0530

    added

--- src/App.tsx
+++ src/App.tsx
@@ -856,7 +856,7 @@ function MainApp() {
                                   setShowCreateJobRole(true);
                                   setShowGuideModal(false);
                                 }}
-                                className="px-3 py-1.5 bg-blue-600 text-white text-sm font-medium rounded-lg hover:bg-blue-700 transition-colors flex items-center absolute top-[-72px] right-[-16px] "
+                                className="px-3 py-1.5 bg-blue-600 text-white text-sm font-medium rounded-lg hover:bg-blue-700 transition-colors flex items-center absolute top-[-72px] right-[64px] "
                               >
                                 Create Role
                               </button>
