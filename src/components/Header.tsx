aa21aa675bd16871a38a9cf29c01cad09afa5161
yuvrajthakur18 <ysthakur180401@gmail.com>
2025-07-31 16:36:11 +0530

    Updated : Linked in search suggestion width fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -105,7 +105,7 @@ const Header: React.FC<HeaderProps> = ({
                     className="bg-transparent text-sm text-gray-700 placeholder-gray-500 focus:outline-none w-40"
                   />
                   {isFocused && candidates.length > 0 && (
-                    <div className="absolute top-full w-[15%] bg-white shadow-lg rounded-lg mt-1 z-10 max-h-40 overflow-y-auto">
+                    <div className="absolute top-full w-full px-3 bg-white shadow-lg rounded-lg mt-1 z-10 max-h-40 overflow-y-auto">
                       {candidates.map((candidate) => (
                         <div
                           key={candidate.id}
