794ccb4fcc428114d76cd896b68914c0838f5de2
yuvrajthakur18 <ysthakur180401@gmail.com>
2025-08-05 15:31:27 +0530

    Testing: Reference tab

--- src/components/CandidateDetail.tsx
+++ src/components/CandidateDetail.tsx
@@ -752,7 +752,7 @@ const CandidateDetail: React.FC<CandidateDetailProps> = ({
                   className="sr-only peer"
                 />
                 <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:bg-blue-600"></div>
-                <div className="absolute left-1 top-1 w-4 h-4 bg-white rounded-full transition-transform peer-checked:translate-x-5"></div>
+                <div className="absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition-transform peer-checked:translate-x-5"></div>
               </label>
             </div>
           </div>
