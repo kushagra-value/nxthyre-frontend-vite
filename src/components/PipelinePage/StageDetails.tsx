348ee77aa20cb25f1e1e1c2e599db05d2c606f0a
yuvrajthakur18 <ysthakur180401@gmail.com>
2025-08-13 18:48:53 +0530

    Interview tab stage data fixes

--- src/components/PipelinePage/StageDetails.tsx
+++ src/components/PipelinePage/StageDetails.tsx
@@ -260,6 +260,11 @@ const StageDetails: React.FC<StageDetailsProps> = ({
   archiveCandidate,
   transferredStageData,
 }) => {
+  console.log(
+    "Selected Candidate::::::::::::::::::::::::::::::::::; ",
+    selectedCandidate
+  );
+  console.log(transferredStageData);
   const [activeTab, setActiveTab] = useState("Profile");
   const [showMoreProfile, setShowMoreProfile] = useState(false);
   const [selectedActivityIndex, setSelectedActivityIndex] = useState<
