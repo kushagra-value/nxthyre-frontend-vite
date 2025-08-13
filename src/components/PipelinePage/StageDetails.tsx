eb4e5c0389ba2f50bc4d9e899dfeb3a125c6a80e
yuvrajthakur18 <ysthakur180401@gmail.com>
2025-08-13 12:46:11 +0530

    Updated : Changed the icon

--- src/components/PipelinePage/StageDetails.tsx
+++ src/components/PipelinePage/StageDetails.tsx
@@ -20,6 +20,7 @@ import {
   Volume2,
   Maximize,
   Play,
+  Send,
 } from "lucide-react";
 
 interface Stage {
@@ -775,20 +776,7 @@ const StageDetails: React.FC<StageDetailsProps> = ({
                 onClick={addActivity}
                 className="bg-blue-500 text-white p-2 rounded-md"
               >
-                <svg
-                  className="w-5 h-5"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M5 13l4 4L19 7"
-                  ></path>
-                </svg>
+                <Send className="w-4 h-4" />
               </button>
             </div>
           </div>
