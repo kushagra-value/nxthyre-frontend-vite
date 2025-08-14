dcdf8558b017290a970783d0c6b2a90584ddb4a3
yuvrajthakur18 <ysthakur180401@gmail.com>
2025-08-14 17:10:25 +0530

    Fixing: UI

--- src/components/PipelinePage/StageDetails.tsx
+++ src/components/PipelinePage/StageDetails.tsx
@@ -791,7 +791,7 @@ const StageDetails: React.FC<StageDetailsProps> = ({
 
         console.log("interviewData Resume score: ", interviewData?.resumeScore);
         return (
-          <div className="space3-y-3 bg-blue-50">
+          <div className="space-y-3 bg-blue-50">
             <div className="bg-white rounded-xl p-2">
               <h4 className="text-base font-medium text-[#4B5563] mb-4">
                 Overall Score
