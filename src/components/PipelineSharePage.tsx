f90d65c49cca9f8378b74e92c90228bec187248c
HarshValuebound <harsh@valuebound.com>
2025-08-19 17:08:46 +0530

    updated PipeLineSharePage section card question

--- src/components/PipelineSharePage.tsx
+++ src/components/PipelineSharePage.tsx
@@ -883,14 +883,14 @@ const ArchiveIcon = () => (
                             score: "4/5",
                           },
                         ].map((item) => (
-                          <div key={item.id} className="bg-[#F5F9FB] border border-gray-200 rounded-lg">
+                          <div key={item.id} className="bg-[#F5F9FB] border border-gray-400 rounded-lg">
                             <div className="flex items-start justify-left gap-4 pb-2 m-4 mb-0">
-                              <span className="text-base font-medium text-gray-700">Q{item.id}.</span>
-                              <p className="text-sm text-gray-600 leading-relaxed">{item.question}</p>
+                              <span className="text-base font-medium text-gray-600">Q{item.id}.</span>
+                              <p className="text-sm text-gray-400 leading-relaxed">{item.question}</p>
                             </div>
-                            <div className="p-4 border border-gray-200 bg-white rounded-tl-lg rounded-tr-lg">
-                            <div className="flex items-center justify-between text-sm text-gray-500 px-6 border-b border-gray-200 py-2">
-                              <div className="text-sm text-gray-600">{item.language}</div>
+                            <div className="px-4 border border-gray-200 bg-white rounded-lg">
+                            <div className="flex items-center justify-between text-sm text-gray-500 ml-2 pl-8 border-b border-gray-200 py-2">
+                              <div className="text-sm text-gray-400">{item.language}</div>
                               <div className="flex items-center gap-3">
                                 <button className="text-gray-400 hover:text-gray-600 flex items-center gap-1">
                                   <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
