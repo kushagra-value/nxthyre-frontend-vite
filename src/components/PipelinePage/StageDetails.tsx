cf57d113a50e5f373ed0fb06ed1d22d256faabe4
yuvrajthakur18 <ysthakur180401@gmail.com>
2025-08-20 14:47:02 +0530

    Fixed: on hover vetted skills adjusting width

--- src/components/PipelinePage/StageDetails.tsx
+++ src/components/PipelinePage/StageDetails.tsx
@@ -1095,7 +1095,7 @@ const StageDetails: React.FC<StageDetailsProps> = ({
                           {skill.rating}
                         </span>
                         {skill.reason && (
-                          <div className="absolute z-50 invisible group-hover:visible opacity-0 group-hover:opacity-100 transition-all duration-200 -top-2 left-1/2 -translate-x-1/2 -translate-y-full bg-blue-100 text-blue-600 text-xs rounded-md py-2 px-3 w-56 overflow-x-visible">
+                          <div className="absolute z-50 invisible group-hover:visible opacity-0 group-hover:opacity-100 transition-all duration-200 -top-2 left-1/2 -translate-x-1/2 -translate-y-full bg-blue-100 text-blue-600 text-xs rounded-md py-2 px-3 w-64 text-center">
                             {skill.reason}
                             <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-full border-4 border-transparent border-t-blue-600"></div>
                           </div>
