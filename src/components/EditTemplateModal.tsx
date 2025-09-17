bb6b71b170d7bcab8e47ae634598af0883142b66
HarshValuebound <harsh@valuebound.com>
2025-09-17 14:17:43 +0530

    updated Edit email template

--- src/components/EditTemplateModal.tsx
+++ src/components/EditTemplateModal.tsx
@@ -880,7 +880,7 @@ const [isAddingFollowUp, setIsAddingFollowUp] = useState(false);
 {showTestEmail && (
           <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-end">
             <div
-              className={`bg-white w-[40%] h-full transform transition-transform duration-300 ease-out p-10 space-y-4 ${
+              className={`bg-white w-full h-full transform transition-transform duration-300 ease-out p-10 space-y-4 ${
                 showTestEmail ? "translate-x-0" : "translate-x-full"
               }`}
             >
