e15e5c8581ff82288746254cf4d0865ff105967a
HarshValuebound <harsh@valuebound.com>
2025-09-17 13:17:25 +0530

    updated Edit email template

--- src/components/EditTemplateModal.tsx
+++ src/components/EditTemplateModal.tsx
@@ -257,7 +257,7 @@ const [isAddingFollowUp, setIsAddingFollowUp] = useState(false);
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-end">
+    <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center">
       <div
         className={`bg-white w-[40%] h-full transform transition-transform duration-300 ease-out p-10 space-y-4 ${
           isOpen ? "translate-x-0" : "translate-x-full"
@@ -803,7 +803,7 @@ const [isAddingFollowUp, setIsAddingFollowUp] = useState(false);
                     Add Follow Up
                   </button>
                 </div>
-              )};
+              )}
               </div>
             </div>
             <div className="flex justify-between mt-6">
