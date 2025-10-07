dbc9bfc48119216621e74752a64b5efda6a0a5d8
HarshValuebound <harsh@valuebound.com>
2025-10-07 11:59:22 +0530

    updating notification

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -311,7 +311,7 @@ const Header: React.FC<HeaderProps> = ({
           </div>
         </div>
       </header>
-     {showPopup && pendingInvites.length > 0 && (
+     {showPopup && (
   <div 
     className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-[9999]"  // UPDATED: Higher z-index for visibility
     onClick={() => setShowPopup(false)}
