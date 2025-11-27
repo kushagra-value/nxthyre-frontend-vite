f1ba0856ca941a4b1be389d9ddcd6caf474768d7
HarshValuebound <harsh@valuebound.com>
2025-11-27 16:57:47 +0530

    updated calender component

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -36,7 +36,7 @@ export const EventCard = ({ event }: EventCardProps) => {
         className="absolute left-0 top-0 bottom-0 w-2 h-full rounded-l-md"
         style={{ backgroundColor: colors.border }}
       />
-      <div className="pl-4 pr-4 py-4 flex flex-col  justify-between h-full">
+      <div className="pl-4 pr-4 py-2 flex flex-col justify-around h-full">
         <div className="flex items-start gap-3">
           {/* Avatar */}
           {event.avatarImageUrl ? (
@@ -65,7 +65,7 @@ export const EventCard = ({ event }: EventCardProps) => {
         </div>
 
         {/* Time + Thumbs Up */}
-        <div className="flex items-center justify-between mt-6 text-base text-[#4B5563]">
+        <div className="flex items-center justify-between text-sm text-[#4B5563]">
           <span className="font-normal">
             {event.startTime} to {event.endTime}
           </span>
