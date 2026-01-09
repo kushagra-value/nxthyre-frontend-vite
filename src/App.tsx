61e5a45254e940cd22b997b73e353d94218a22ce
HarshValuebound <harsh@valuebound.com>
2026-01-09 12:45:10 +0530

    updated project card

--- src/App.tsx
+++ src/App.tsx
@@ -125,12 +125,14 @@ interface Workspace {
 
 const ProjectSkeletonCard = () => (
     <div className="bg-white rounded-[10px] shadow-lg overflow-hidden animate-pulse">
-      <div className="p-8">
-        <div className="h-8 bg-gray-200 rounded-lg mb-4"></div>
+      <div className="p-4">
+        <div className="h-8 bg-gray-200 rounded-lg mb-6"></div>
         <div className="h-4 bg-gray-200 rounded w-4/5 mb-4"></div>
-        <div className="flex flex-wrap gap-4 mb-4">
-          <div className="h-6 bg-gray-200 rounded-full w-32"></div>
-          <div className="h-6 bg-gray-200 rounded-full w-28"></div>
+        <div className="flex flex-wrap gap-4 mb-4 w-1/2">
+          <div className="h-6 bg-gray-200 rounded-full w-6"></div>
+          <div className="h-6 bg-gray-200 rounded-full w-6"></div>
+          <div className="h-6 bg-gray-200 rounded-full w-6"></div>
+          <div className="h-6 bg-gray-200 rounded-full w-6"></div>
         </div>
       </div>
     </div>
