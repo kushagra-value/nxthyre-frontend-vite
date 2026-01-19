3f2e8deeb3705b92975a75484e5168db8dbe7e33
yuvrajthakur18 <ysthakur180401@gmail.com>
2026-01-19 18:45:02 +0530

    fixing buttons

--- src/components/PipelineStages.tsx
+++ src/components/PipelineStages.tsx
@@ -3109,7 +3109,7 @@ const PipelineStages: React.FC<PipelineStagesProps> = ({
                                   <svg
                                     width="16"
                                     height="15"
-                                    viewBox="0 0 16 15"
+                                    viewBox="0 0 18 17"
                                     fill="none"
                                     xmlns="http://www.w3.org/2000/svg"
                                   >
