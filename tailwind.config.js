e8c7c571df9d663c8c8b3131a904f371e50bac4b
HarshValuebound <harsh@valuebound.com>
2026-01-21 16:13:04 +0530

    updated hidding logic of source

--- tailwind.config.js
+++ tailwind.config.js
@@ -61,6 +61,9 @@ export default {
           "0%": { transform: "translateY(10px)", opacity: "0" },
           "100%": { transform: "translateY(0)", opacity: "1" },
         },
+        screens: {
+          '3xl': '1920px',
+        },
       },
     },
   },
