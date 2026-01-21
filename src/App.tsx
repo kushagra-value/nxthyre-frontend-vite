0619b74b49f5cbb20461445e34b38a9c43937f87
HarshValuebound <harsh@valuebound.com>
2026-01-21 14:08:28 +0530

    feat: introduce the main application component with routing, authentication, and global state management.

--- src/App.tsx
+++ src/App.tsx
@@ -526,7 +526,6 @@ function MainApp() {
         maxTotalExp: job.experience_max_years
           ? job.experience_max_years.toString()
           : "",
-        locations: job.location ? job.location.filter(Boolean) : [],
         application_type: activeTab,
         sort_by: sortBy,
       };
@@ -1531,7 +1530,7 @@ function MainApp() {
 
                           return (
                             <>
-                              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                              <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 mx-auto">
                                 {currentCategories.map((job) => (
                                   <div
                                     key={job.id}
