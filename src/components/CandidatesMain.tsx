1fdad1cddc1fa3ebcf89ef42c53513e71b4438b8
yuvrajthakur18 <ysthakur180401@gmail.com>
2026-01-27 18:19:33 +0530

    handle resume reveal

--- src/components/CandidatesMain.tsx
+++ src/components/CandidatesMain.tsx
@@ -2077,7 +2077,6 @@ const CandidatesMain: React.FC<CandidatesMainProps> = ({
                                 if (url) {
                                   window.open(url, "_blank");
                                 } else {
-                                  handleConfirmReveal();
                                   setPendingReveal({
                                     candidateId: candidate.id,
                                     onSuccess: (prem) => {
@@ -2088,6 +2087,7 @@ const CandidatesMain: React.FC<CandidatesMainProps> = ({
                                     },
                                   });
                                   // setShowRevealDialog(true);
+                                  handleConfirmReveal();
                                 }
                               }}
                               aria-label={`View ${candidate.full_name}'s resume`}
