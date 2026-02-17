c4625b2a3d8eb0f5158f3afba78f642bd1ff8d6a
yuvrajthakur18 <ysthakur180401@gmail.com>
2026-02-17 13:43:15 +0530

    Updating the icon

--- src/components/applicantTracking/ShareCandidateListPage.tsx
+++ src/components/applicantTracking/ShareCandidateListPage.tsx
@@ -310,12 +310,12 @@ const ShareCandidateListPage: React.FC<ShareCandidateListPageProps> = ({
                           >
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
-                              width="24"
-                              height="24"
+                              width="20"
+                              height="20"
                               viewBox="0 0 24 16"
                               fill="none"
                               stroke="currentColor"
-                              stroke-width="1"
+                              stroke-width="2"
                               stroke-linecap="round"
                               stroke-linejoin="round"
                               className="lucide lucide-arrow-up-right-icon lucide-arrow-up-right"
