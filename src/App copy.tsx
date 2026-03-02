c3d2a28850d87cff80dddc1b7fa878c92915b7dc
HarshValuebound <harsh@valuebound.com>
2026-03-02 15:54:23 +0530

    "CosmeticChanges-V2-0 updated structure of code

--- src/App copy.tsx
+++ src/App copy.tsx
@@ -61,7 +61,7 @@ import {
 import { showToast } from "./utils/toast";
 import CandidateBackGroundCheck from "./pages/CandidateBackGroundCheck";
 import SuperAdminDashboard from "./components/SuperAdmin/SuperAdminDashboard";
-import ProjectCard from "./pages/jobs/components/ProjectCard";
+import ProjectCard from "./components/ProjectCard";
 import { AnalysisResult } from "./services/candidateService";
 import ShareCandidateListPage from "./components/applicantTracking/ShareCandidateListPage";
 interface Category {
