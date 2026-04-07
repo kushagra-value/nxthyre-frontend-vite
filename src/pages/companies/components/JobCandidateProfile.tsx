57a622a7898d108f8c104dd7c2e7773e7ccaaf91
HarshValuebound <harsh@valuebound.com>
2026-04-08 00:19:41 +0530

    Update SVG icon size in JobCandidateProfile component for improved visual consistency and responsiveness.

--- src/pages/companies/components/JobCandidateProfile.tsx
+++ src/pages/companies/components/JobCandidateProfile.tsx
@@ -1241,7 +1241,7 @@ export default function JobCandidateProfile({
                       className="border border-[#E5E7EB] rounded-lg p-3 bg-white flex items-center justify-between group cursor-pointer hover:border-[#0F47F2] transition"
                     >
                       <div className="flex items-center gap-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="10" height="10" viewBox="0 0 48 48">
                           <path fill="#2196f3" d="M37,45H11c-1.657,0-3-1.343-3-3V6c0-1.657,1.343-3,3-3h19l10,10v29C40,43.657,38.657,45,37,45z"></path><path fill="#bbdefb" d="M40 13L30 13 30 3z"></path><path fill="#1565c0" d="M30 13L40 23 40 13z"></path><path fill="#e3f2fd" d="M15 23H33V25H15zM15 27H33V29H15zM15 31H33V33H15zM15 35H25V37H15z"></path>
                         </svg>
                         <span className="font-bold text-xs text-black line-clamp-1 truncate w-40">
