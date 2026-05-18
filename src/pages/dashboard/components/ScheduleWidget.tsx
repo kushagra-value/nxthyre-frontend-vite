a46e792a9e739f1d0ed910b61262f4dc259ccf63
HarshValuebound <harsh@valuebound.com>
2026-05-18 18:31:42 +0530

    Reverting Changes

--- src/pages/dashboard/components/ScheduleWidget.tsx
+++ src/pages/dashboard/components/ScheduleWidget.tsx
@@ -107,7 +107,7 @@ export default function ScheduleWidget({ events, isLoading, onEventClick, active
       <div className="overflow-y-auto max-h-[314px] hide-scrollbar px-5 pb-5">
         <div className="relative flex flex-col gap-2.5">
           <div
-            className="absolute left-[112px] top-0 bottom-0 w-0"
+            className="absolute left-[115px] top-0 bottom-0 w-0"
             style={{ borderLeft: '1px dashed #D1D1D6' }}
           />
 
