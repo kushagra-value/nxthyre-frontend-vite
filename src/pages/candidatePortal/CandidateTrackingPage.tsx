77a90b9c11a1c052a88ead78ff95abc3e8318f30
rohits95724 <rohit.s@valuebound.com>
2026-05-19 13:22:41 +0530

    feat:added

--- src/pages/candidatePortal/CandidateTrackingPage.tsx
+++ src/pages/candidatePortal/CandidateTrackingPage.tsx
@@ -365,6 +365,7 @@ const CandidateTrackingPage = () => {
   console.log("check that are we getting the recuiter name now ",recruiterName)
 
   const recruiterPhone = getRecruiterPhone(recruiterName);
+  console.log("check the recuiter phone number",recruiterPhone)
   const whatsappUrl = recruiterPhone ? getWhatsAppUrl(recruiterPhone) : "https://wa.me";
 
   console.log("check the whatsapp url ",whatsappUrl)
